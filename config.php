b5f9821e59d671fe9ca283665dd4793a7c3af147
Md. Imran Hossain Shaon <mdshaonimran@gmail.com>
2010-10-20 04:15:48 +0600

    added configuration file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * config.php
+ * @author Md. Imran Hossain Shaon (mdshaonimran)
+ * @version 1.00
+ */
+
+define('CONSUMER_KEY', 'YOUR APPLICATION KEY');
+define('CONSUMER_SECRET', 'YOUR APPLICATION SECRET');
+define('ACCESS_TOKEN', 'YOUR ACCESS TOKEN');
+define('ACCESS_TOKEN_SECRET', 'YOUR ACCESS TOKEN SECRET');
+
+define('FEED_URL', 'http://mdshaonimran.wordpress.com/feed/');
+
+?>
