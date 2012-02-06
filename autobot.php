3f478d8ac1d801ddcc000484bc4abf0575d6b38c
shaon <shaon@MacBook-Pro.local>
2012-02-06 19:04:08 +0600

    comments added

--- autobot.php
+++ autobot.php
@@ -1,4 +1,30 @@
 <?php
+
+/**
+*
+* This is the main file of oauthtwitterbot
+*
+* @author       Md. Imran Hossain Shaon (mdshaonimran@gmail.com)
+* @version      1.0
+* @copyright    2011 Md. Imran Hossain Shaon 
+* @license      GNU GPL
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+* 
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>
+*
+*/
+
+
 require_once 'twitteroauth/twitteroauth.php';
 require_once 'config.php';
 require 'simplepie.inc';
